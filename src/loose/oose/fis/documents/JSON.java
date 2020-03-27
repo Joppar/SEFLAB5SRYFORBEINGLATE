ba510802c7c002a6fcc79121b16ecfc31367cb21
Joppar <heavypirate@yahoo.com>
2020-03-27 18:10:34 +0200

    the 2 branches was merged and the first one has been deleted

---
+++ src/loose/oose/fis/documents/JSON.java
@@ -0,0 +1,12 @@
+package loose.oose.fis.documents;
+
+import java.util.Arrays;
+
+public class JSON extends Document {
+    }
+
+    @Override
+    public String toString() {
+        return "JSON " + super.toString();
+    }
+}
